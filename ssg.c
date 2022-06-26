6c60d005e418faca41dd3e83b7e1922dc8cc5620
rayan nakib <dev-rayan@proton.me>
2022-06-26 12:05:02 +0100

    ssg init
    create new website with sssg init

--- ssg.c
+++ ssg.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main (int argc, char *argv[])
-{
-  printf("Hello, World!\n");
-  return 0;
+void help() {
+  printf("   ssg init\tto init a website\n");
 }
+
+void init() {
+  system("mkdir src dst");
+  system("mkdir src/pages src/images src/css");
+  system("touch src/index.html src/_header src/_footer");
+  system("touch src/css/global.css src/css/header.css src/css/footer.css");
+  system("git init");
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc == 2 && strcmp(argv[1], "init") == 0)
+    init();
+  else
+    help();
+
+  return 0; }
